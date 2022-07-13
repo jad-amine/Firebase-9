2068d300c5c53c3b3bda48c0055a4227c58402e4
jad amine <jade1992amine@gmail.com>
2022-07-13 06:50:40 +0300

    Front End: Setting up Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello from index.js");
+// ==== Firebase 8 we used to import all firebase/app part of the library to only use the function initializeApp =============
+// (import firebase from 'firebase/app'
+// firebase.initializeApp(firebaseConfig))
+
+// Firebase 9 // only import one function
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUJKVJmrQ8PxDeXYmIG19VcFRo8jHCgBg",
+  authDomain: "fir-9-start-4ffc9.firebaseapp.com",
+  projectId: "fir-9-start-4ffc9",
+  storageBucket: "fir-9-start-4ffc9.appspot.com",
+  messagingSenderId: "663584947171",
+  appId: "1:663584947171:web:f4b47ad2a8aaee1a663ed6",
+};
+
+initializeApp(firebaseConfig);
